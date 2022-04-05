63daffd30431e3c00ed16a2218c3436645e016e1
Maunzi <46403167+M4unz1@users.noreply.github.com>
2022-04-05 02:55:57 +0200

    Added 2Captcha Support

--- namesforreddit.py
+++ namesforreddit.py
@@ -1,3 +1,4 @@
+from cgitb import reset
 from os.path import dirname
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,13 +9,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import random
 import time
-import re
 import string
 import secrets
 import os
+import sys
+from twocaptcha import TwoCaptcha
+
+api_key= 'API_KEY'
+url = 'https://www.reddit.com/register/'
+
 driver = webdriver.Chrome(ChromeDriverManager().install()) # USES CHROMEDRIVERMANAGER TO AUTO UPDATE CHROMEDRIVER
 
+
+solver = TwoCaptcha(api_key)
 # GENERATE PASSWORD
+
 alphabet = string.ascii_letters + string.digits
 password = ''.join(secrets.choice(alphabet) for i in range(16))
 # PASSWORD GENERATION FINISHED
@@ -45,7 +54,7 @@ time.sleep(1)
 # NAME GENERATION FINISHED
 
 # REDDIT ACCOUNT CREATION
-driver.get('https://www.reddit.com/register/')
+driver.get(url)
 driver.find_element(By.ID, 'regEmail').send_keys('mail@mail.mail')
 time.sleep(1)
 driver.find_element(By.XPATH, "//button[contains(text(),'Fortsetzen')]").click()
@@ -53,6 +62,36 @@ time.sleep(3)
 driver.find_element(By.ID, 'regUsername').send_keys(finalName)
 driver.find_element(By.ID, 'regPassword').send_keys(password)
 
-WebDriverWait(driver, 40).until(EC.visibility_of_element_located((By.CLASS_NAME, "recaptcha-checkbox-checkmark")))
+#WebDriverWait(driver, 40).until(EC.visibility_of_element_located((By.CLASS_NAME, "recaptcha-checkbox-checkmark")))
+time.sleep(10)
+sitekey = driver.find_element(By.XPATH,'/html/body/script[1]').get_attribute('outerHTML')
+sitekey_clean= sitekey.split("""';
+
+                window.___r""")[0].split("window.___grecaptchaSiteKey = '")[1]
+print(sitekey)
+print (sitekey_clean)
+try:
+    result = solver.recaptcha(
+        sitekey=sitekey_clean,
+        url=url)
+
+except Exception as e:
+    sys.exit(e)
+
+else:
+    print('result: ' + str(result))
+
+    
+    result1= result['code']
+    print(result1)
+    driver.execute_script('var element=document.getElementById("g-recaptcha-response"); element.style.display="";')
+
+    driver.execute_script("""document.getElementById("g-recaptcha-response").innerHTML = arguments[0]""", result1)
+    driver.execute_script('var element=document.getElementById("g-recaptcha-response"); element.style.display="none";')
+
+    driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div/div/div[3]/button').click()
+    
+    time.sleep(5)
+
 
 # driver.close()
